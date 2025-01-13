fb4dfd6c07bb36989faaf20274bb59fd105f63b4
utkarsh <utkarshmalik19@gmail.com>
2025-01-13 23:21:30 +0530

    Initial commit

---
+++ src/main/java/com/utkarsh/social_media_app/SocialMediaAppApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.social_media_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarsh/social_media_app/SocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.social_media_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
